c265aec7072fac067f95944715d576a562a5c4ee
vladimT <91445179+vladimT@users.noreply.github.com>
2023-09-01 05:09:12 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+valid_phone = os.getenv('valid_phone')
+valid_email = os.getenv('valid_email')
+valid_password = os.getenv('valid_password')
+valid_login = os.getenv('valid_login')
